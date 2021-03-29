f7413b53429ed3cc56dd48d60e2e8f2e8a43625a
Ben Mohammad <benmohammad593@gmail.com>
2021-03-29 09:11:08 +0100

    TodoAdapter.kt complete...

--- app/src/main/java/com/benmohammad/mvi2/screens/todolist/TodoAdapter.kt
+++ app/src/main/java/com/benmohammad/mvi2/screens/todolist/TodoAdapter.kt
@@ -3,18 +3,25 @@ package com.benmohammad.mvi2.screens.todolist
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.benmohammad.mvi2.R
-import com.benmohammad.mvi2.TodoListState
-import com.benmohammad.mvi2.TodoListStore
+import com.benmohammad.mvi2.*
 import com.benmohammad.mvi2.mvi.StateSubscriber
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.plusAssign
 import javax.inject.Inject
 
 class TodoAdapter @Inject constructor(
     private val taskIntentFactory: TodoIntentFactory,
     private val store: TodoListStore
 ): RecyclerView.Adapter<TodoVH>(), StateSubscriber<TodoListState> {
+
+    private val disposables = CompositeDisposable()
+    private lateinit var filteredTodoList: List<TodoEntity>
+
+    init {
+        setHasStableIds(true)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoVH {
         val rootView = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
@@ -24,14 +31,34 @@ class TodoAdapter @Inject constructor(
     }
 
     override fun onBindViewHolder(holder: TodoVH, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(filteredTodoList[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return filteredTodoList.size
     }
 
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        disposables += store.modelState().subscribeToState()
+    }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        disposables.clear()
+    }
+
+
+
     override fun Observable<TodoListState>.subscribeToState(): Disposable {
-        TODO("Not yet implemented")
+        return map {
+            (todoList, filter) ->
+            todoList.filter { task -> filter.filter(task) }
+        }.distinctUntilChanged().subscribe(::updateList)
+    }
+
+    private fun updateList(todoList: List<TodoEntity>) {
+        filteredTodoList = todoList
+        notifyDataSetChanged()
     }
 }
